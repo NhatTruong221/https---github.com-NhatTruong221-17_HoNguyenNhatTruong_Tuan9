06d05bcd451a0eb9b5e0881c14e74ae1c67011d6
NhatTruong221 <78464609+NhatTruong221@users.noreply.github.com>
2023-11-19 16:07:00 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+
+/*function reducer(state = { count: 1}, action){
+  switch (action.type){
+    case "plus":
+      return {
+        count: state.count + 1,
+      };
+    case "minus":
+      return {
+        count: state.count - 1,
+      };
+
+    default: 
+      return state;
+  }
+}*/
+
+function reducer(state = [], action){
+  switch (action.type){
+    case "plus":
+      return [...state, ' Nhật Trường']
+    case "minus":
+      return [...state, ' Đang học']
+
+    default: 
+      return state;
+  }
+}
+
+
+var store = createStore(reducer);
+
+console.log(store);
+store.dispatch({type:'plus'});
+console.log(store.getState());
+
+/*store.dispatch({type:'plus'});
+console.log(store.getState());*/
+
+store.dispatch({type:'minus'});
+console.log(store.getState());
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+
     </View>
   );
 }
